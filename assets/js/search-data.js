32782ca2a193803dc5df69fd0a728864f3c0ddaf
dtran421 <duketran2001@gmail.com>
2025-02-26 06:18:43 +0000

    Deploying to gh-pages from @ dtran421/dtran421.github.io@1c5edb3a146c06f9dbc4032239f0808f21960a03 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,17 +25,17 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "These are some projects that I&#39;ve worked on in the past.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+        },{id: "nav-resume",
+          title: "resume",
+          description: "You can download a copy of my resume by clicking the blue PDF button above.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/resume/";
           },
         },{id: "post-a-post-with-image-galleries",
       
